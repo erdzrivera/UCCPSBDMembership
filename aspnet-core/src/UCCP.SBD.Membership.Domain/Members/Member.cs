ae639758931e4688eb279af3097c93a4acd22b11
Erdie Rivera <Erdie.Rivera@nowcom.com.ph>
2026-01-27 23:29:35 +0800

    feat: Add Member entity, MemberDto, and CreateUpdateMemberDto for membership management.

--- aspnet-core/src/UCCP.SBD.Membership.Domain/Members/Member.cs
+++ aspnet-core/src/UCCP.SBD.Membership.Domain/Members/Member.cs
@@ -12,10 +12,10 @@ namespace UCCP.SBD.Membership.Members
         public string Occupation { get; set; }
         public string BaptismDate { get; set; }
         public string BaptizedBy { get; set; }
-        public string PlaceOfBirth { get; set; }
-        public string FatherName { get; set; }
-        public string MotherName { get; set; }
-        public string Sponsors { get; set; }
+        public string? PlaceOfBirth { get; set; }
+        public string? FatherName { get; set; }
+        public string? MotherName { get; set; }
+        public string? Sponsors { get; set; }
         
         public string MemberTypeId { get; set; }
         public string OrganizationId { get; set; }
@@ -33,10 +33,10 @@ namespace UCCP.SBD.Membership.Members
             string occupation, 
             string baptismDate, 
             string baptizedBy, 
-            string placeOfBirth,
-            string fatherName,
-            string motherName,
-            string sponsors,
+            string? placeOfBirth,
+            string? fatherName,
+            string? motherName,
+            string? sponsors,
             string memberTypeId, 
             string organizationId, 
             bool isActive) 
